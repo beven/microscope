505d8199f794a9835ab33e903abe2120f03e92cc
Beven Lee <bevenl@gmail.com>
2014-11-23 21:47:09 +1100

    Modified app to load posts from MongoDB instead of hardcoded posts.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,24 +1,10 @@
 /**
  * Created by blee on 23/11/2014.
  */
-
-var postData = [
-    {
-        title: 'Introducing telescope',
-        url: 'http://something.com'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor book',
-        url: 'http://themeterorbook.com'
-    }
-];
-
 Template.postsList.helpers({
-        posts: postData
+        posts: function(){
+            return Posts.find();
+        }
     }
 );
 
