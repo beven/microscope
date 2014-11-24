4b6be9653ca00aff531f55e7fe8418cd54d6df3c
Beven Lee <bevenl@gmail.com>
2014-11-24 23:51:58 +1100

    Added a submit post page and created a link to it in the header.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,9 @@
 Meteor.publish('posts', function() {
     return Posts.find();
 });
+
+//Meteor.startup(function() {
+//    Tracker.autorun(function() {
+//        console.log('There are ' + Posts.find().count() + ' posts');
+//    });
+//});
